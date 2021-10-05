209eb5dfa8c9902a9239138ce3477fd15e7e4049
Andressa Cantelli <andressa-cantelli@outlook.com>
2021-10-05 17:41:28 -0300

    Sprint 03

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -5,12 +5,15 @@ Quando for na rota /, execute a função:  res.send
 - O express devolve 2 coisas: req (requisição que a pessoa está enviando) e res (o que estamos devolvendo)
 É necessário exportar o app.get através do método module.exports */
 
+const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
-    app.get('/atendimentos',(req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
+    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
 
-    app.post('/atendimentos',(req, res) => {
-        console.log(req.body)
-        res.send('Você está na rota de atendimentos e está realizando um POST')
+    app.post('/atendimentos', (req, res) => {
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento)
+        res.send('Post atendimento')
     })
 }
--- index.js
+++ index.js
@@ -3,10 +3,23 @@
 /* Invés de exportar o express direto, exporta-se o file customExpress, 
 que exporta o módulo c/ a arrow function: */
 const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao')
+const Tabelas = require('./infraestrutura/tabelas')
 
-//Declara que o app = function customExpress
-const app = customExpress()
+conexao.connect(erro => {
+    if(erro) {
+        console.log(erro)
+    } else {
+        console.log('conectado com sucesso')
 
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
-/* Subir no servidor
-          Porta, Função  */
+        Tabelas.init(conexao)
+        
+        //Declara que o app = function customExpress
+        const app = customExpress()
+
+        app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+        /* Subir no servidor
+                Porta, Função  */
+    }
+})
+
